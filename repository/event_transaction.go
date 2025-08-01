fc50a07cb7b14e2cdf930d44a5de3f95a83283e9
Gemilang Cahyaning Adi <gemilang_adi@kecilin.id>
2025-08-01 09:32:42 +0700

    fix paid_at

--- repository/event_transaction.go
+++ repository/event_transaction.go
@@ -185,7 +185,7 @@ func (r *EventTransactionRepositoryImpl) MarkTransactionAsSuccess(ctx context.Co
 	currentTime := time.Now()
 	defer cancel()
 
-	query := `UPDATE event_transactions SET transaction_status = $1, updated_at = $2, pg_order_id = $3 WHERE id = $4 RETURNING id, created_at`
+	query := `UPDATE event_transactions SET transaction_status = $1, paid_at = $2, pg_order_id = $3 WHERE id = $4 RETURNING id, created_at`
 	if tx != nil {
 		err = tx.QueryRow(ctx, query, lib.EventTransactionStatusSuccess, currentTime, pgOrderID, transactionID).Scan(&res.ID, &res.CreatedAt)
 	} else {
